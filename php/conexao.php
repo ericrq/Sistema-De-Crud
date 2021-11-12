131caf3e67fd24d207b0b3e65112830026609d5a
Eric <ericbm30@outlook.com>
2021-11-12 04:09:48 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -3,7 +3,7 @@
 //conexao com o banco mysql
 $connection = mysqli_connect('localhost', 'root', '', 'crud');
 
-//executa a conexao
+// verifica se houve erro
 if (mysqli_connect_errno()) {
     // se houver erro 
     echo "Error connecting to database";
